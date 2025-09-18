9081d9265152df41f479058dac4ea6ce2dc190ac
Devgressai <229847365+Devgressai@users.noreply.github.com>
2025-09-17 17:28:35 -0700

    build(tsconfig): target modern ES2020 to avoid legacy polyfills; tweak font preconnect

--- app/layout.tsx
+++ app/layout.tsx
@@ -130,7 +130,7 @@ export default function RootLayout({
         <meta name="msapplication-config" content="/browserconfig.xml" />
         
         {/* Preload critical resources */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="//fonts.googleapis.com" />
         <link rel="dns-prefetch" href="//fonts.gstatic.com" />
